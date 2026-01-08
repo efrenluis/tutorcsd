cd0c880c0fe84115b8e51d9764d5c10c1c51d169
efrenluis <efrenlpc@gmail.com>
2026-01-08 01:43:40 +0000

    Update app.py

--- app.py
+++ app.py
@@ -122,7 +122,7 @@ def ask_gemini(prompt, api_key, system_role):
         PREGUNTA DEL USUARIO: {prompt}
         """
         
-        model = genai.GenerativeModel('gemini-1.5-flash')
+        model = genai.GenerativeModel('models/gemini-1.5-flash')
         response = model.generate_content(master_prompt)
         return response.text
     except Exception as e:
